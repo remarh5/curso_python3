2be67f18aa3cc01a2c6b2b6820dc08ce132c037e
remarh5 <45855011+remarh5@users.noreply.github.com>
2021-07-28 15:47:11 -0500

    Agregar practica 1 curso de python

---
+++ practica1.py
@@ -0,0 +1,37 @@
+
+"""
+eje1
+fecha = int(19091978)
+nombre = "Rene"
+unir = nombre + str(fecha)
+print (f"Mi nombre es {nombre} y mi fech de nacimiento es el año: {fecha} las concatenacion es: {unir}")
+
+eje2
+mes = int(9)
+cumpli_anio = 7 > 9
+print (f"Este anio ya cumpli anios? {cumpli_anio}")
+
+eje3
+gusta_gatos = False
+gusta_perros = True
+gusta_ga_pe = gusta_gatos and gusta_perros
+print (f"Es verdad que me gusta los gatos y los perros? {gusta_ga_pe}")
+
+
+eje4
+nombre = input("Cuale es tu nombre: ")
+mes_cumple = int(input("Cual es el mes de tu cumple años: "))
+primeros_seis_meses_anio = (1,2,3,4,5,6)
+cumplo = mes_cumple in primeros_seis_meses_anio
+print(f"Hola mi nombre es {nombre}. Cumplo años en los primeros meses del año? {cumplo}")
+
+eje5
+nombre = input("Cuale es tu nombre: ")
+mes_cumple = int(input("Cual es el mes de tu cumple años: "))
+mi_info = {"nombre": nombre, "mes_de_nacimiento": mes_cumple}
+primeros_seis_meses_anio = (1,2,3,4,5,6)
+cumplo = mi_info["mes_de_nacimiento"] in primeros_seis_meses_anio
+print(F"Hola mi nombre es {mi_info['nombre']}. Cumplo años en los primeros meses del año? {cumplo}")
+"""
+
+
